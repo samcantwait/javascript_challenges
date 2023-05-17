4207cb57875b0081bb9a0ac47f9f0c671192a5aa
Samantha Atkins <samatkins@gmail.com>
2023-05-17 16:09:13 -0500

    add recursive projects

---
+++ power.js
@@ -0,0 +1,9 @@
+function power(base, exp) {
+    if (exp === 0) return 1;
+    return base * power(base, exp - 1);
+}
+
+console.log(power(2, 8))
+console.log(power(2, 0)) // 1
+console.log(power(2, 2)) // 4
+console.log(power(2, 4)) // 16
